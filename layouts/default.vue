355b996f494a7b510353e347d4061f4642e46310
Stephen Bau <bauhouse@gmail.com>
2019-07-16 16:05:17 -0700

    Add main-nav component to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,63 +1,6 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            Design Influences
-          </a>
-
-          <div class="navbar-burger">
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </div>
-        </div>
-
-        <div id="main-menu" class="navbar-menu">
-          <div class="navbar-start">
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">Imagine</a>
-              <div class="navbar-dropdown">
-                <a class="navbar-item">Resources</a>
-                <a class="navbar-item">Theory</a>
-                <a class="navbar-item">Research</a>
-                <a class="navbar-item">Strategy</a>
-              </div>
-            </div>
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">Design</a>
-              <div class="navbar-dropdown">
-                <a class="navbar-item">Identity</a>
-                <a class="navbar-item">Print</a>
-                <a class="navbar-item">Web</a>
-                <a class="navbar-item">Type</a>
-              </div>
-            </div>
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">Build</a>
-              <div class="navbar-dropdown">
-                <a class="navbar-item">People</a>
-                <a class="navbar-item">Relationships</a>
-                <a class="navbar-item">Community</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="navbar-end">
-            <a class="navbar-item">About</a>
-            <a class="navbar-item">Journal</a>
-            <a class="navbar-item">Work</a>
-            <a class="navbar-item">Resources</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-
+    <main-nav />
     <section class="main-content columns">
       <aside class="column is-2 section">
         <p class="menu-label is-hidden-touch">
@@ -71,7 +14,6 @@
           </li>
         </ul>
       </aside>
-
       <div class="container column is-10">
         <nuxt />
       </div>
@@ -80,6 +22,8 @@
 </template>
 
 <script>
+import MainNav from '~/components/MainNav'
+
 export default {
   data() {
     return {
@@ -96,6 +40,9 @@ export default {
         }
       ]
     }
+  },
+  components: {
+    MainNav
   }
 }
 </script>
