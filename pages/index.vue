bc177fb8839e77065131ff416b64394acbf89355
Stephen Bau <bauhouse@gmail.com>
2019-07-17 06:56:36 -0700

    Add hero component to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="container">
-    <section class="section">
-      <div class="columns is-mobile">
-        <card title="Imagine" icon="triangle">
-          Events
-        </card>
+  <div class="index">
+    <hero />
+    <div class="container">
+      <section class="section">
+        <div class="columns is-mobile">
+          <card title="Imagine" icon="triangle">
+            Events
+          </card>
 
-        <card title="Design" icon="circle">
-          Workshops
-        </card>
+          <card title="Design" icon="circle">
+            Workshops
+          </card>
 
-        <card title="Build" icon="square">
-          Projects
-        </card>
-      </div>
-    </section>
+          <card title="Build" icon="square">
+            Projects
+          </card>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 import Card from '~/components/Card'
+import Hero from '~/components/Hero'
 
 export default {
   name: 'HomePage',
 
   components: {
-    Card
+    Card,
+    Hero
   }
 }
 </script>
