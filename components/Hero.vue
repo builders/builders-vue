6ba04543f6b1477ceb127a703d9d56627eb1c2a7
Stephen Bau <bauhouse@gmail.com>
2019-07-17 06:56:09 -0700

    Create Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Design Influences
+        </h1>
+        <h2 class="subtitle">
+          Inspired by the past. Shaping the future.
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
