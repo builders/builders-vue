d00351c6889e57d055a177af09017e2419db30b3
Stephen Bau <bauhouse@gmail.com>
2019-07-17 14:49:55 -0700

    Replace inspire page with imagine page

---
+++ pages/imagine/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.page.imagine
+  hero(title='Imagine' subtitle='Creativity and innovation at the intersection of art and science.' image='https://source.unsplash.com/nBWmOsOdi4Y/1600x900' image-alt='Bauhaus Archive, Berlin')
+    .container
+</template>
+
+<script>
+import Hero from '~/components/Hero'
+
+export default {
+  name: 'Imagine',
+
+  components: {
+    Hero
+  }
+}
+</script>
