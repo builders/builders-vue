b2af2d5bb0ee29894694b1a631ea5a3093dfe55b
Stephen Bau <bauhouse@gmail.com>
2019-07-17 06:36:28 -0700

    Modify cards: Imagine, Design, Build

--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,16 @@
   <div class="container">
     <section class="section">
       <div class="columns is-mobile">
-        <card title="Free" icon="github-circle">
-          Open source on
-          <a href="https://github.com/buefy/buefy">
-            GitHub
-          </a>
+        <card title="Imagine" icon="triangle">
+          Events
         </card>
 
-        <card title="Responsive" icon="cellphone-link">
-          <b class="has-text-grey">
-            Every
-          </b>
-          component is responsive
+        <card title="Design" icon="circle">
+          Workshops
         </card>
 
-        <card title="Modern" icon="alert-decagram">
-          Built with
-          <a href="https://vuejs.org/">
-            Vue.js
-          </a>
-          and
-          <a href="http://bulma.io/">
-            Bulma
-          </a>
-        </card>
-
-        <card title="Lightweight" icon="arrange-bring-to-front">
-          No other internal dependency
+        <card title="Build" icon="square">
+          Projects
         </card>
       </div>
     </section>
