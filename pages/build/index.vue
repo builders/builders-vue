3f6534e1fb209b99c8ffd65fe9d5a90e9da67185
Stephen Bau <bauhouse@gmail.com>
2019-07-17 23:56:10 -0700

    Background classes for build and design pages

--- pages/build/index.vue
+++ pages/build/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .page.imagine
-  hero(title='Build' subtitle='Building resilience, relationships, and community.' image='https://source.unsplash.com/JUHbj2ZV5hE/1600x900' image-alt='Bauhaus Building, Dessau')
+  hero(title='Build' subtitle='Building resilience, relationships, and community.' image='https://source.unsplash.com/JUHbj2ZV5hE/1600x900' image-alt='Bauhaus Building, Dessau' background-class='is-tertiary')
     .container
 </template>
 
--- pages/design/index.vue
+++ pages/design/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .page.imagine
-  hero(title='Design' subtitle='Improving the experience of all living things on Earth.' image='https://source.unsplash.com/nrhnluxL1E8/1600x900' image-alt='Bauhaus Archive, Berlin')
+  hero(title='Design' subtitle='Improving the experience of all living things on Earth.' image='https://source.unsplash.com/nrhnluxL1E8/1600x900' image-alt='Bauhaus Archive, Berlin' background-class='is-secondary')
     .container
 </template>
 
