4cf5cb2b398cc2af477e760bc833d3c34c2cc925
Stephen Bau <bauhouse@gmail.com>
2019-09-09 21:10:18 -0700

    Add Journal _slug template

---
+++ pages/journal/_slug.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="article journal">
+    <hero-section
+      title="Journal"
+      subtitle="Progress and the evolution of ideas."
+      image="https://source.unsplash.com/CxYHfBkC0vs/1600x900"
+      image-alt="Filling a blank page"
+      color="secondary"
+      size="is-medium"
+    />
+    <journal-article :attributes="attributes" :html="html" />
+  </div>
+</template>
+
+<script>
+import HeroSection from '~/components/HeroSection'
+import JournalArticle from '~/components/JournalArticle'
+
+export default {
+  name: 'JournalSlug',
+
+  components: {
+    HeroSection,
+    JournalArticle
+  },
+  async asyncData({ params, route }) {
+    const articleName = params.slug
+    const markdownContent = await import(`~/contents/journal/${articleName}.md`)
+    return {
+      attributes: markdownContent.attributes,
+      html: markdownContent.html,
+      currentUrl: route.path
+    };
+  }
+};
+</script>
