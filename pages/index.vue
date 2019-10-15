fed1d5dfa00da709e5c15ccf676f2f38b830ba1b
Stephen Bau <bauhouse@gmail.com>
2019-10-15 05:51:21 -0700

    Home page markup

--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,216 @@
 <template>
-  <div class="page index">
-    <hero-section
-      title="Design Influences"
-      subtitle="Inspired by the past. Shaping the future."
-      image="/images/tel-aviv-yafo-israel-R6cG5_u-Lkw.jpg"
-      image-alt="Bograshov St 1, Tel Aviv-Yafo, Israel"
-    />
-    <section-intro
-      title="A New Synthesis"
-      subtitle="The past century has been marked by division. We need a new synthesis."
-    >
-      <p class="intro-paragraph">
-        In the service of industrial models of social and political engineering,
-        art and technology have been co-opted to participate in a
-        military-industrial complex that is a synthesis of our worst human
-        characteristics: greed and hatred.
-      </p>
-      <p class="intro-paragraph">
-        We are caught in a dialectic of opposing forces, in the politics of
-        thesis and antithesis, right versus left. We need a new synthesis that
-        transcends the old paradigms. Unity in diversity is the model for both
-        the organism and the planet.
-      </p>
-      <p class="intro-paragraph">
-        <strong>The new synthesis is integrating biology and physics.</strong>
-        The future of design and architecture may be in the integration of the
-        separate disciplines to learn how interconnected systems work and model
-        the built environment to imitate the processes of living organisms. We
-        are beginning to see these changes happening through the self-organizing
-        principles of biological adaptation.
-      </p>
-    </section-intro>
-    <section-journal
-      title="Journal"
-      subtitle="Progress and the evolution of ideas."
-      :articles="articles"
-    />
-    <section class="section is-medium">
+  <main>
+    <section class="section">
       <div class="container">
         <div class="columns is-variable is-6">
-          <icon-card
-            title="Imagine"
-            icon="circle"
-            image="/images/circle.svg"
-            color="primary"
-            link="/imagine"
-          >
-            Events
-          </icon-card>
-          <icon-card
-            title="Design"
-            icon="triangle"
-            image="/images/triangle.svg"
-            color="secondary"
-            link="/design"
-          >
-            Workshops
-          </icon-card>
-          <icon-card
-            title="Build"
-            icon="square"
-            image="/images/square.svg"
-            color="tertiary"
-            link="/build">
-            Projects
-          </icon-card>
+          <div class="column">
+            <div class="card card-imagine">
+              <div class="card-image">
+                <figure class="image">
+                  <a href="#"><img src="https://source.unsplash.com/m73iqlbI-3M/960x960" alt="Bauhaus Archiv, Berlin photo by Simone Hutsch" /></a>
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <div class="meta"><a class="author" href="#">David Jones</a></div>
+                  <h3><a href="#">The Structure of Storytelling and the Art and Craft of World Building for Film</a></h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card card-design">
+              <div class="card-image">
+                <figure class="image">
+                  <a href="#"><img src="https://source.unsplash.com/tvPvROBv0F4/960x960" alt="Evening over Millennium Bridge photo by James Padolsey" /></a>
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <div class="meta"><a class="author" href="#">Sandi Co</a></div>
+                  <h3><a href="#">Bringing Design Education to a New Generation with a Global Mentoring Platform</a></h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card card-build">
+              <div class="card-image">
+                <figure class="image">
+                  <a href="#"><img src="https://source.unsplash.com/czDvRp5V2b0/960x960" alt="Abu Dhabi Louvre Museum by Alvaro Pinot" /></a>
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <div class="meta"><a class="author" href="#">Brad Smith</a></div>
+                  <h3><a href="#">Construction, Deconstruction and Reconstruction: Exploring the Process of Community Building</a></h3>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-  </div>
+    <section id="design" class="section section-image has-bg-img" style="background-image: url(https://source.unsplash.com/c9z9RlCh0Zo/1920x1280)">
+      <div class="section-body">
+        <div class="container">
+          <div class="columns is-variable is-6">
+            <div class="column is-offset-4 is-two-thirds">
+              <h1 class="title">
+                Design for Resilience
+              </h1>
+              <h2 class="subtitle">
+                Reimagining our social architecture in the  context of humanity + technology at scale
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="imagine" class="section is-medium">
+      <div class="container">
+        <div class="columns is-variable is-6">
+          <div class="column">
+            <figure class="image">
+              <span class="img-border"><img src="https://source.unsplash.com/324nknuhxDg/960x960" alt="Photo from Unsplash" /></span>
+              <figcaption><strong>Art + Science:</strong> at the intersection of creativity and technology</figcaption>
+            </figure>
+          </div>
+          <div class="column">
+            <p>The iconic images for me as a child were Walt Disney on television and the other
+            was Albert Einstein, who had embodied this thing about understanding the world.
+            I grew up liking both of those, but the deepest thing that I wanted to do was
+            become an animator. I did a lot of drawing all the way through high school. I
+            even had a class where I got an A plus, plus, plus. Now, I know that was based
+            more on quantity than quality.</p>
+            <p>As I went from high school to go to college, I realized that I didn’t know how
+            to get the skills and it was not up to the level that I saw in the Disney
+            animators, so I switched over to Physics.</p>
+          </div>
+          <div class="column">
+            <p>Here’s the interesting thing I found. I’ve told this story before, as you would
+            guess, over the years, and there’s always a titter and laughter in the audience,
+            because the general concept that we have is to go from art to science is
+            incongruous.</p>
+            <p>Why is it incongruous to go from one to the other and why do we think of them as
+            two very different personalities? I will say that over the years, I have known
+            world-class mathematicians and scientists and artists and programmers, and if I
+            were to take another characteristic, let’s say it’s their organizational skills,
+            the ability to get stuff done. I haven’t found a correlation between them.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="build" class="section">
+      <div class="container">
+        <div class="columns is-variable is-6">
+          <div class="column is-one-third section-heading">
+            <h2 class="title">builders collective</h2>
+          </div>
+        </div>
+        <div class="columns is-variable is-6">
+          <div class="column is-two-thirds">
+            <p>A gathering of people who are investing their collective time, energy  and
+            resources in the challenge of reimagining our social architecture.</p>
+          </div>
+          <div class="column">
+            <div class="control">
+              <button class="button is-medium is-link">
+                <strong>Join Us</strong>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-variable is-6">
+          <div class="column">
+            <figure class="image">
+              <img class="is-rounded" src="https://source.unsplash.com/uGXEEwnvIkQ/256x256" alt="People photos from Unsplash" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image">
+              <img class="is-rounded" src="https://source.unsplash.com/2llsUy9LP50/256x256" alt="People photos from Unsplash" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image">
+              <img class="is-rounded" src="https://source.unsplash.com/2EGNqazbAMk/256x256" alt="People photos from Unsplash" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image">
+              <img class="is-rounded" src="https://source.unsplash.com/LirPcFdrJkE/256x256" alt="People photos from Unsplash" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image">
+              <img class="is-rounded" src="https://source.unsplash.com/Ml4tr2WO7JE/256x256" alt="People photos from Unsplash" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image">
+              <img class="is-rounded" src="https://source.unsplash.com/9R-CH7PR150/256x256" alt="People photos from Unsplash" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="community" class="section is-medium">
+      <div class="container">
+        <div class="columns is-variable is-6">
+          <div class="column">
+            <a href="#events">
+              <figure class="image has-text-centered">
+                <img class="shape circle" src="images/circle.svg" alt="circle" />
+                <figcaption><h2 class="title">events</h2></figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="column">
+            <a href="#workshops">
+              <figure class="image has-text-centered">
+                <img class="shape triangle" src="images/triangle.svg" alt="triangle" />
+                <figcaption><h2 class="title">workshops</h2></figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="column">
+            <a href="#projects">
+              <figure class="image has-text-centered">
+                <img class="shape square" src="images/square.svg" alt="square" />
+                <figcaption><h2 class="title">projects</h2></figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="connect" class="section">
+      <div class="container">
+        <div class="columns is-vcentered is-variable is-6">
+          <div class="column">
+            <p class="title">connect</p>
+            <p class="subtitle is-4 has-text-grey-light">Keep in touch and stay informed about the latest events, workshops and projects.</p>
+          </div>
+          <div class="column">
+            <form action="#subscribe" method="POST" accept-charset="utf-8">
+              <div class="field is-grouped">
+                <div class="control has-icons-left is-expanded">
+                  <input type="email" value="" name="email" class="input is-medium is-flat" placeholder="email address" required="">
+                  <span class="icon is-small is-left">
+                    <svg class="svg-inline--fa fa-envelope fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg><!-- <i class="fas fa-envelope"></i> -->
+                  </span>
+                </div>
+                <div class="control">
+                  <button class="button is-medium is-link">
+                    <strong>Subscribe</strong>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
-
-<script>
-import IconCard from '~/components/IconCard'
-import HeroSection from '~/components/HeroSection'
-import SectionIntro from '~/components/SectionIntro'
-import SectionJournal from '~/components/SectionJournal'
-
-import articles from '~/contents/journal/articles.js'
-
-export default {
-  name: 'HomePage',
-
-  components: {
-    IconCard,
-    HeroSection,
-    SectionIntro,
-    SectionJournal
-  },
-  async asyncData ({ route }) {
-    const promises = articles.map(article => import(`~/contents/journal/${article}.md`))
-    return { articles: await Promise.all(promises) }
-  }
-}
-</script>
